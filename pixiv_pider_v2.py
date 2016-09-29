7f87831eba82f2b0e83729099080bc4d2f168df9
Yi <1693338569@qq.com>
2016-09-29 20:56:44 +0800

    threading, oop

--- pixiv_pider_v2.py
+++ pixiv_pider_v2.py
@@ -9,6 +9,7 @@ import urllib
 import urllib2
 import requests
 import cookielib
+import threading
 from bs4 import BeautifulSoup
 
 class Pixiv(object):
@@ -44,7 +45,7 @@ class Pixiv(object):
         pattern = re.compile('(?<=<input type="hidden" name="post_key" value=")\w*(?=">)')
         item = re.findall(pattern, content)
         if not item:
-            print  "postkey is not found"
+            print ('postkey is not found')
             exit()
         post_key = str(item[0])
 
@@ -54,12 +55,29 @@ class Pixiv(object):
         with open ('cookie.txt', 'wb') as f:
             pickle.dump(m_cookies, f)
 
+
+
+    class MyThread(threading.Thread):
+        def __init__(self,pic_url,pic_name,headers):
+            threading.Thread.__init__(self)
+            self.url = pic_url
+            self.filename = pic_name
+            self.headers = headers
+        def run(self):
+            res = urllib2.Request(self.url,headers=self.headers)
+            req = urllib2.urlopen(res)
+            picture = req.read()
+            filename = self.filename + '.png'
+            with open(filename, 'wb') as f:
+                f.write(picture)
+                f.close()
+
     def get_pic_number_list(self,url):
         number_list =[]
         res = requests.get(url,cookies=load_cookies('cookie.txt'), headers=self.headers_base)
-        print res.status_code
+        # print(res.status_code)
         if res == None:
-            print "can't open the %s page" %(url)
+            print ("can't open the %s page" %(url))
             exit()
         page = res.text
         pattern_number = re.compile(r'</div></a><a href="(.*?)"><h1 class="title" title="(.*?)">.*?</h1></a>',re.S)
@@ -69,19 +87,29 @@ class Pixiv(object):
             number_list.append(url)
         return number_list
 
-    def get_pic_url(self,url_pic_page):
+    def get_pic_url(self,pic_page_url):
         pic_url = ''
         pic_name = ''
-        res = requests.get(url_pic_page,cookies=load_cookies('cookie.txt'),headers=self.headers_base)
+        res = requests.get(pic_page_url,cookies=load_cookies('cookie.txt'),headers=self.headers_base)
         pic_page = res.text
         soup = BeautifulSoup(pic_page,"lxml")
         tags = soup.find_all("img", attrs={"class": "original-image"})
         for item in tags:
             pic_url = item['data-src']
             pic_name = item['alt']
-
         return pic_url,pic_name
 
+
+    def download_pic(self,pic_page_url):
+        pic_url, pic_name = self.get_pic_url(pic_page_url)
+        if pic_url:                                                     # 若是单图
+            self.headers_download['referer'] = pic_page_url
+            name = pic_name
+            mt = self.MyThread(pic_url,name,self.headers_download)
+            mt.start()
+            return pic_name
+        else:
+            return 'None'
 def load_cookies(filename):
     with open(filename, 'rb') as f:
         return pickle.load(f)
@@ -93,15 +121,17 @@ def save_cookies(requests_cookiejar, filename):
 
 pixiv_id = ''
 password = ''
-fouce_url = 'http://www.pixiv.net/bookmark_new_illust.php'
-pixiv = Pixiv(fouce_url,pixiv_id,password)
-
+base_fouce_url = 'http://www.pixiv.net/bookmark_new_illust.php'
+number = 100
+pixiv = Pixiv(pixiv_id,password)
 pixiv.login()
-pic_number_list = pixiv.get_pic_number_list(fouce_url)
-print pic_number_list
 
-for item in pic_number_list:
-    src, name = pixiv.get_pic_url(item)
-    print src
-    print name
-    time.sleep(2)
+Num = 1
+for n in range(70,80):
+    fouce_url = base_fouce_url + '?p=' + str(n)
+    pic_number_list = pixiv.get_pic_number_list(fouce_url)
+    for item in pic_number_list:
+        try:
+            filename = pixiv.download_pic(item)
+            print (filename + ' the NO.%d   page%d')  %(Num,n)
+        Num += 1
